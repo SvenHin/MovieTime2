483aea6d6d55e6a20dea8a4b1c113dd0f3ac5f17
haheskja <Cyth0DS>
2018-10-04 15:34:21 +0200

    Big code cleanup

--- MovieTime2/App_Start/BundleConfig.cs
+++ MovieTime2/App_Start/BundleConfig.cs
@@ -13,18 +13,24 @@ namespace MovieTime2
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/mainmoviejquery").Include(
+                        "~/Scripts/MainMovieLoader.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/cartjquery").Include(
+                        "~/Scripts/CartLoader.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryandajax").Include(
+                        "~/Scripts/jquery-3.3.1.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalidation").Include(
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
         }
     }
 }

394ed838fb7f951c685654deee1a42840dd2728c
Kristoffer Dagestad <kristoffer.daggy1@gmail.com>
2018-10-16 15:12:59 +0200

    qwer

---
+++ DAL/adminDALStub.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieTime2.Models;
+
+namespace DAL
+{
+    class adminDALStub
+    {
+        public List<Customer> getAllCustomers()
+        {
+            var customerList = new List<Customer>();
+            var customer = new Customer()
+            {
+                FirstName = "Gunnar",
+                LastName = "Raggsson",
+                Address = "Kjellandgata",
+                Location = "Oslo",
+                ZipCode = "1234",
+                PhoneNumber = "46765643",
+                Email = "Gunnar_Raggsson@gmail.com",
+                Username = "Gusson",
+                Password = "Password123",
+
+            };
+            customerList.Add(customer);
+            customerList.Add(customer);
+            customerList.Add(customer);
+            return customerList;
+        }
+
+        public bool deleteCustomer (int id)
+        {
+            if (id == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}

d0d76677f91d7e3b9b077c1bc90810428b8c5738
Kristoffer Dagestad <kristoffer.daggy1@gmail.com>
2018-10-26 14:40:17 +0200

    sert

--- UnitTest/AdminControllerTest.cs
+++ UnitTest/AdminControllerTest.cs
@@ -139,7 +139,7 @@ namespace MovieTime2.UnitTest
             Assert.AreEqual(result, "true");
         }
         [TestMethod]
-        public void test_editMovieNoid()
+        public void test_editMovieNoiID()
         {
             var controller = new AdminController(new AdminLogic(new AdminDALStub()));
             var movie = new movie()
@@ -152,78 +152,51 @@ namespace MovieTime2.UnitTest
                 genre = "Action",
                 genre2 = "Comedy"
             };
-            
-             result = 
+
+          string result = controller.editMovie(movie);
+
+            Assert.AreEqual(result, "[-1,-1,-1,-1,-1]");
+
 
         }
         [TestMethod]
-        public void test_editMovieTTitle()
+        public void test_editMovieWithiID()
         {
             var controller = new AdminController(new AdminLogic(new AdminDALStub()));
-
             var movie = new movie()
             {
                 id = 1,
-                title = "Something"
+                title = "Totoro",
+                summary = "eowkgnåweg",
+                price = 123,
+                imageURL = "sdfghjklø",
+                genre = "Action",
+                genre2 = "Comedy"
             };
-
 
             string result = controller.editMovie(movie);
 
-            Assert.AreEqual(result, "true");
+            Assert.AreEqual(result, "[1,1,1,1,1]");
+
 
         }
         [TestMethod]
-        public void test_editMovieTSummary()
+        public void test_editMovieWithIDNoInput()
         {
             var controller = new AdminController(new AdminLogic(new AdminDALStub()));
-
             var movie = new movie()
             {
                 id = 1,
-                summary = "something something"
+                
             };
-
 
             string result = controller.editMovie(movie);
 
-            Assert.AreEqual(result, "true");
+            Assert.AreEqual(result, "[0,0,0,0,0]");
+
 
         }
-        [TestMethod]
-        public void test_editMovieTPrice()
-        {
-            var controller = new AdminController(new AdminLogic(new AdminDALStub()));
 
-            var movie = new movie()
-            {
-                id = 1,
-                price = 100
-            };
-
-
-            string result = controller.editMovie(movie);
-
-            Assert.AreEqual(result, "true");
-
-        }
-        [TestMethod]
-        public void test_editMovieTImageURL()
-        {
-            var controller = new AdminController(new AdminLogic(new AdminDALStub()));
-
-            var movie = new movie()
-            {
-                id = 1,
-                imageURL = "sdfghjkl"
-            };
-
-
-            string result = controller.editMovie(movie);
-
-            Assert.AreEqual(result, "true");
-
-        }
         [TestMethod]
         public void test_searchMovie()
         {
@@ -251,6 +224,28 @@ namespace MovieTime2.UnitTest
 
         }
         //Customer Methods Below
+
+        [TestMethod]
+        public void test_editCustomerNoiID()
+        {
+            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
+            var customer = new ListCustomer()
+            {
+                Id = 0,
+                FirstName = "Gunnar",
+                LastName = "Raggsson",
+                Address = "Kjellandgata",
+                Location = "Oslo",
+                ZipCode = "1234",
+                PhoneNumber = "46765643",
+                Email = "Gunnar_Raggsson@gmail.com",
+                Username = "Gusson"
+            };
+            string result = controller.editCustomer(customer);
+
+            Assert.AreEqual(result, "[-1,-1,-1,-1,-1,-1,-1]");
+
+        }
 
         [TestMethod]
         public void test_getAllCustomers()
@@ -303,138 +298,6 @@ namespace MovieTime2.UnitTest
             Assert.AreEqual(result, "true");
         }
         
-        [TestMethod]
-        public void test_editFirstNameT()
-        {
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                FirstName = "Firstname"
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-
-
-        }
-        
-        [TestMethod]
-        public void test_editLastNameT() {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                LastName = "Lastname"
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-        }
-        
-        [TestMethod]
-        public void test_editUsernameT() {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                Username = "Username123"
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-        }
-        
-        [TestMethod]
-        public void test_editAddressT() {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                Address = "Testveien 43"
-
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-        }
-        
-        [TestMethod]
-        public void test_editPhoneNumberT() {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                PhoneNumber = "11223344"
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-        }
-        
-        [TestMethod]
-        public void test_editEmailT() {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                Email = "test@test.com"
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
-        }
-        [TestMethod]
-        public void test_CustomerF()
-        {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 0,
-                
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "false");
-        }
-        [TestMethod]
-        public void test_editZipCodeAndLocation()
-        {
-
-            var controller = new AdminController(new CustomerLogic(new CustomerDALStub()));
-
-            var customer = new ListCustomer()
-            {
-                Id = 1,
-                ZipCode = "1234",
-                Location = "Oslo"
-
-
-            };
-
-            string result = controller.editCustomer(customer);
-
-            Assert.AreEqual(result, "true");
         }
 
         [TestMethod]
